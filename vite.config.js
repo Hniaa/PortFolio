e283fc24866da9a1d8c85d4a3650f4edcc4fb46b
Hniaa <haniadekimeche@gmail.com>
2026-01-31 17:31:10 +0100

    mAj dépôt

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Remplace 'mon-portfolio-pixel' par le NOM EXACT de ton repo GitHub
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PortFolio/', 
 })
